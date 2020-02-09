cd5bfececee9552e0dd31acdf9bc88c608fec358
pathiratk <pathirat.kos@gmail.com>
2020-02-08 21:27:54 -0800

    remove folder

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import haxe.Template;
 import flixel.FlxState;
 
 class PlayState extends FlxState
@@ -7,9 +8,18 @@ class PlayState extends FlxState
 	override public function create():Void
 	{
 		super.create();
+
+		// Welcome text
 		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
 		text.screenCenter();
 		add(text);
+
+		// TODO: add start menu
+
+		// TODO: add tilemap
+
+		// TODO: add sprites
+		
 	}
 
 	override public function update(elapsed:Float):Void
