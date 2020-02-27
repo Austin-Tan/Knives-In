e68b3984336a97bf68180275a46dfe74de1ba0e3
pathiratk <pathirat.kos@gmail.com>
2020-02-26 18:17:48 -0800

    add drop-off script

--- analytics/iter-2/drop-off-time-graph.py
+++ analytics/iter-2/drop-off-time-graph.py
@@ -2,12 +2,22 @@ import csv
 import sys
 import pdb
 
-UID_INDEX = 3
-uid_map = {}
+index = {
+   'cid': 0,
+   'uid': 1,
+   'sessionid': 2,
+   'qid': 3,
+   'dqid': 4,
+   'q-s_id': 5,
+   'log_q_ts': 6,
+   'q_detail': 7
+}
 
 file = csv.reader(sys.stdin, dialect=csv.excel)
+
+uid_map = {}
 for line in file:
-   uid = line[UID_INDEX]
+   uid = line[index['uid']]
    if uid == 'uid':
       continue
    if not uid in uid_map:
@@ -15,31 +25,25 @@ for line in file:
    uid_map[uid].append(line)
 
 session_to_time = {}
-returns_arr = []
-
 for key, value in uid_map.items():
    start_time = ''
    end_time = ''
    session_map = {}
 
-
    for data in value:
-      session_id = data[18]
+      session_id = data[index['sessionid']]
       if not session_id in session_map:
          session_map[session_id] = []
       session_map[session_id].append(data)
 
-   returns = len(session_map)
-   returns_arr.append(returns)
-
    for k, v in session_map.items():
-      start_time = min([int(line[2]) for line in v])
-      end_time = max([int(line[2]) for line in v])
+      start_time = min([int(line[index['log_q_ts']]) for line in v])
+      end_time = max([int(line[index['log_q_ts']]) for line in v])
       duration = int(end_time) - int(start_time)
       session_to_time[k] = duration
 
 time_map = {}
-for time in range(0, 575, 10):
+for time in range(0, 600, 10):
    count = 0
    for k, v in session_to_time.items():
       if v > time:
@@ -47,16 +51,6 @@ for time in range(0, 575, 10):
    time_map[time] = count
    print('{},{}'.format(time, count))
 
-# return_map = {}
-# for time in range(0, 20, 1):
-#    count = 0
-#    for v in returns_arr:
-#       if v == time:
-#          count += 1
-#    return_map[time] = count
-#    print('{},{}'.format(time, count))
-
-   
 
 
 
