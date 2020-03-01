249ac93be125e6a402bc74a3be1c4e8f8cd2b937
pathiratk <pathirat.kos@gmail.com>
2020-02-29 17:32:18 -0800

    add pat's user id

--- analytics/iter-2/actions-query.sql
+++ analytics/iter-2/actions-query.sql
@@ -1,4 +1,6 @@
 select a.cid, a.uid, b.sessionid, a.qid, a.dqid, a.aid, a.qaction_seq_id, a.log_ts, a.a_details 
 from player_actions_log as a left join player_quests_log as b 
 on a.dqid = b.dqid
-and a.cid = 2;
+and a.cid = 2
+and a.uid != '1EA827E9-D91B-BCA4-54F7-4BF7160F7467'
+and a.uid != 'BD3557C9-3D10-3A0C-5CC7-03F4AD599437';
--- analytics/iter-2/quests-query.sql
+++ analytics/iter-2/quests-query.sql
@@ -1,3 +1,5 @@
 select cid, uid, sessionid, qid, dqid, q_s_id, log_q_ts, q_detail 
 from player_quests_log
-where cid = 2;
+where cid = 2
+and uid != '1EA827E9-D91B-BCA4-54F7-4BF7160F7467'
+and uid != 'BD3557C9-3D10-3A0C-5CC7-03F4AD599437';
