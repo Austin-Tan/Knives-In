564be9c30b0470cff9b3bbe07dff72bae79e0f17
pathiratk <pathirat.kos@gmail.com>
2020-03-02 21:34:12 -0800

    reorganize analysis folder

--- analytics/iter-2/queries/actions-query.sql
+++ analytics/iter-2/queries/actions-query.sql
@@ -1,6 +1,7 @@
-select a.cid, a.uid, b.sessionid, a.qid, a.dqid, a.aid, a.qaction_seq_id, a.log_ts, a.a_details 
+select a.cid, a.uid, b.sessionid, a.qid, a.dqid, a.aid, a.qaction_seqid, a.log_ts, a.a_detail 
 from player_actions_log as a left join player_quests_log as b 
 on a.dqid = b.dqid
-and a.cid = 2
+where a.cid = 2
+or a.cid = -3
 and a.uid != '1EA827E9-D91B-BCA4-54F7-4BF7160F7467'
 and a.uid != 'BD3557C9-3D10-3A0C-5CC7-03F4AD599437';
--- analytics/iter-2/queries/quests-query.sql
+++ analytics/iter-2/queries/quests-query.sql
@@ -1,4 +1,6 @@
-select uid, sessionid, log_ts from player_pageload_log
+select cid, uid, sessionid, qid, dqid, q_s_id, log_q_ts, q_detail 
+from player_quests_log
 where cid = 2
+or cid = -3
 and uid != '1EA827E9-D91B-BCA4-54F7-4BF7160F7467'
 and uid != 'BD3557C9-3D10-3A0C-5CC7-03F4AD599437';
